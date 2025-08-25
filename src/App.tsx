4e5996c96a43729aabf3ac15a541c6631e1aa228
Lucas Caracoci <lucascaracoci2@gmail.com>
2025-08-25 17:34:14 -0300

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { Button } from "./components/Button"
+import {ThemeProvider} from 'styled-components'
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 function App() {
 
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button/>
       <Button variant="secondary"/>
       <Button variant="danger"/>
       <Button variant="success"/>
       <Button variant="primary"/>
  
-    </>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
